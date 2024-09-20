41d848b7adbfe93ccee0adafd9a7972ba2e9c14a
Lagom92 <jyyt0147@gmail.com>
2024-09-20 16:33:12 +0900

    Edit

--- week6/1260-yang.py
+++ week6/1260-yang.py
@@ -2,39 +2,7 @@
 백준 1260번: DFS와 BFS
 https://www.acmicpc.net/problem/1260
 
-
-
 '''
-
-
-# 시간 초과 발생함
-def func(arr, visited, snode, mode='BFS'):
-    if mode == 'BFS':
-        n = 0
-    else:    # 'DFS'
-        n = -1
-
-    box = [snode]    # 스택용
-    res = []    # 결과반환용
-
-    while box:
-        node = box.pop(n)
-        if str(node) not in res:
-            res.append(str(node))
-
-        for idx, val in enumerate(arr):
-            if (node in val) and (visited[idx] == False):
-                
-                visited[idx] = True
-                box.append(val[0])
-                box.append(val[1])
-                break
-
-    return ' '.join(res)
-
-
-# 아래 함수는 통과됨
-
 def BFS(arr, visited, snode):
     box = [snode]    # 스택용
     res = []    # 결과반환용
@@ -66,19 +34,18 @@ def DFS(arr, visited, snode):
     return ' '.join(map(str, res))
 
 def main():
-    # N, M, V = 4, 5, 1
-    # arr = [[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]]
+    # ex) N, M, V = 4, 5, 1
+    # ex) arr = [[1, 2], [1, 3], [1, 4], [2, 4], [3, 4]]
 
     N, M, V = list(map(int, input().split()))
-
-    # arr = list( list(map(int, input().split())) for _ in range(M))
-    # arr.sort()
 
     arr = [[] for _ in range(N+1)]
     for _ in range(M):
         a, b = map(int, input().split())
         arr[a].append(b)
         arr[b].append(a)
+    
+    # ex) arr = [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
 
     visited = [False]*(N+1)
     res_dfs = DFS(arr, visited, snode=V)
@@ -88,8 +55,10 @@ def main():
    
     print(res_dfs)
     print(res_bfs)
-    
 
+    # ex) 1 2 4 3
+    # ex) 1 2 3 4
+    
 
 
 if __name__=="__main__":
